f099df7d86454a382be9634c50b58148413485cc
xnjanx02 <anjana.jk009@gmail.com>
2022-07-08 01:32:58 +0530

    New component view-doctors added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { SearcgDepartmentComponent } from './searcg-department/searcg-department.component';
+import { ViewDoctorsComponent } from './view-doctors/view-doctors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
-    SearcgDepartmentComponent
+    SearcgDepartmentComponent,
+    ViewDoctorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-doctors/view-doctors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-doctors',
+  templateUrl: './view-doctors.component.html',
+  styleUrls: ['./view-doctors.component.css']
+})
+export class ViewDoctorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
